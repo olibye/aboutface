693faab1ed7a5f813c928d688575f5f607525bab
Oli Bye <oliver.bye@byhiras.com>
2013-02-18 10:56:57 +0000

    Row Bean

--- src/main/java/com/positiverobot/aboutface/TableBean.java
+++ src/main/java/com/positiverobot/aboutface/TableBean.java
@@ -8,36 +8,61 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
 
 import org.primefaces.event.RowEditEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@ManagedBean(name="tableBean")
+@ManagedBean(name = "tableBean")
 public class TableBean {
 
-	private List<String> model = new ArrayList();
+    private static final Logger LOG = LoggerFactory.getLogger(TableBean.class);
 
-	{
-		model.add("Row 1");
-		model.add("Row 2");
-		model.add("Row 3");
-		model.add("Row 4");
-		model.add("Row 5");
-		model.add("Row 6");
-		model.add("Row 7");
-		model.add("Row 8");
-		model.add("Row 9");
-		model.add("Row 10");
-	}
+    private List<Row> model = new ArrayList();
 
-	public TableBean() {
-	}
+    {
+        model.add(new Row("Row 1"));
+        model.add(new Row("Row 2"));
+        model.add(new Row("Row 3"));
+        model.add(new Row("Row 4"));
+        model.add(new Row("Row 5"));
+        model.add(new Row("Row 6"));
+        model.add(new Row("Row 7"));
+        model.add(new Row("Row 8"));
+        model.add(new Row("Row 9"));
+        model.add(new Row("Row 10"));
+    }
 
-	public List<String> getModel() {
-		System.out.println(FacesContext.getCurrentInstance().getCurrentPhaseId());
-		return model;
-	}
+    public class Row {
+        String value;
 
-	public void onEdit(RowEditEvent event) {
-	}
+        public Row(String value) {
+            super();
+            this.value = value;
+        }
 
-	public void onCancel(RowEditEvent event) {
-	}
+        public String getValue() {
+            LOG.info("In Phase:{}", FacesContext.getCurrentInstance().getCurrentPhaseId());
+            return value;
+        }
+
+        public void setValue(String value) {
+            LOG.info("In Phase:{}", FacesContext.getCurrentInstance().getCurrentPhaseId());
+            this.value = value;
+        }
+    }
+    
+    public TableBean() {
+    }
+
+    public List<Row> getModel() {
+        LOG.info("In Phase:{}", FacesContext.getCurrentInstance().getCurrentPhaseId());
+        return model;
+    }
+
+    public void onEdit(RowEditEvent event) {
+        LOG.info("In Phase:{}", FacesContext.getCurrentInstance().getCurrentPhaseId());
+    }
+
+    public void onCancel(RowEditEvent event) {
+        LOG.info("In Phase:{}", FacesContext.getCurrentInstance().getCurrentPhaseId());
+    }
 }
